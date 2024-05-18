264f3946e612fddea6990092605fb8d92285b963
ilya <uiv@coolclever.ru>
2024-05-18 23:19:04 +0300

    Main UI

--- app/src/main/java/com/example/fabrikatepla/presentation/ui/home/CategoryItem/CategoryScreen.kt
+++ app/src/main/java/com/example/fabrikatepla/presentation/ui/home/CategoryItem/CategoryScreen.kt
@@ -1,14 +1,19 @@
 package com.example.fabrikatepla.presentation.ui.home.CategoryItem
 
 import CatalogGridHotKeysRender
+import DarkThemeColors.SubText1
 import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,8 +22,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -39,12 +45,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.fabrikatepla.R
 import com.example.fabrikatepla.data.CategoryItem
@@ -131,14 +140,22 @@ fun CategoryScreen(
                         state = rememberStateScroll,
                         modifier = Modifier.padding(paddingValues),
                         contentPadding = it
-                    ){
+                    ) {
                         item {
-                            Column (
+                            Column(
                                 modifier = Modifier.padding(top = 10.dp)
-                            ){
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(
+                                            start = 15.dp,
+                                            bottom = 10.dp
+                                        ),
+                                    text = "Популярное",
+                                    fontSize = 14.sp
+                                )
                                 CatalogGridHotKeysRender(
-                                    listHotkeys = getCatalogGridHotKeys().catalogTab
-                                    ,
+                                    listHotkeys = getCatalogGridHotKeys().catalogTab,
                                     state = state,
                                     onItemClick = {
                                         selectedItem.value = it
@@ -147,116 +164,72 @@ fun CategoryScreen(
                                 )
                                 CatalogGridRecommendationRender(
                                     getCatalogGridRecommendation().catalogCarousel,
-                                    onClickItem = {headCategory, categoryId -> }
+                                    onClickItem = { headCategory, categoryId -> }
                                 )
                                 CatalogGridRootSectionsRender(
                                     getCatalogGridSections().catalogCarousel,
                                     onClickItem = { item -> },
                                     onClickShowAllInSection = { id, name -> }
                                 )
+                                ContinueBoxes()
                             }
                         }
                     }
-
-
-//                    LazyColumn(
-//                        state = rememberStateScroll,
-//                        modifier = Modifier.padding(paddingValues),
-//                        contentPadding = it
-//                    ) {
-//                        item {
-//                            Text(
-//                                text = "Категории",
-//                                modifier = Modifier.fillMaxWidth().padding(top = 20.dp, bottom = 15.dp),
-//                                textAlign = TextAlign.Center,
-//                                fontSize = 24.sp
-//                            )
-//                        }
-//                        state.list.forEach { item ->
-//                            item {
-//                                Commodity(
-//                                    item = item,
-//                                    onItemClick = {
-//                                        selectedItem.value = it
-//                                        viewModel.updateId(it.id)
-//                                    }
-//                                )
-//                            }
-//                        }
-//                        item {
-//                            Text(
-//                                text = "Горячие скидки",
-//                                modifier = Modifier
-//                                    .fillMaxWidth()
-//                                    .padding(top = 50.dp),
-//                                textAlign = TextAlign.Center,
-//                                fontSize = 24.sp
-//                            )
-//                        }
-//                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Commodity(
-    item: Item,
-    onItemClick: (Item) -> Unit
-) {
-    Card(
-        modifier = Modifier.padding(5.dp)
-    ){
-        Card(
-            border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
-            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
-            onClick = { onItemClick(item) }
+fun ContinueBoxes() {
+    Column {
+        Text(
+            modifier = Modifier
+                .padding(start = 15.dp),
+            text = "Все категории",
+            fontSize = 14.sp
+        )
+        Column (
+            modifier = Modifier
+                .height(700.dp)
+                .padding(3.dp)
+            ,
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
-            Title(item = item)
-            Image(
-                painter = rememberAsyncImagePainter(
-                    item.imageSrc
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(250.dp),
-                contentDescription = null
-            )
-            Box(Modifier.padding(15.dp)) {
-                Text(text = item.description,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
-            }
+            Boxes()
+            Boxes()
+            Boxes()
+            Boxes()
+            Boxes()
         }
     }
 
 }
 
+
 @Composable
-fun Title(item: Item) {
+fun Boxes() {
     Row(
-        Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround
     ) {
-        Spacer(modifier = Modifier.width(8.dp))
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = item.name,
-                color = Color(0xFFFA6C37),
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-        }
-        Image(
-            imageVector = Icons.Rounded.MoreVert,
-            contentDescription = null
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .height(120.dp)
+                .width(180.dp)
+                .clickable {}
+                .background(SubText1, RoundedCornerShape(16.dp)),
+        )
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .height(120.dp)
+                .width(180.dp)
+                .clickable {}
+                .background(SubText1, RoundedCornerShape(16.dp)),
         )
     }
 }
