29a49c89bffba17537e7975442fd150226829d9c
ilya <uiv@coolclever.ru>
2024-05-26 18:53:50 +0300

    color

--- app/src/main/java/com/example/fabrikatepla/ui/theme/Color.kt
+++ app/src/main/java/com/example/fabrikatepla/ui/theme/Color.kt
@@ -8,4 +8,4 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val ProfileIconBackground = Color(0xFFF3F2F1)
-val ProfilePlasticCardBackground = Color(0xFFDBFE01)
+val ProfilePlasticCardBackground = Color(0xFFFFECA7)
