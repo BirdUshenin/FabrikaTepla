a78e37b8ad53955bc59167fbced50d843e97c019
andre <okmyanskiy.andrey@gmail.com>
2024-05-26 17:38:35 +0200

    Profile screen: bottom sheet is now going through a configuration change

--- app/src/main/java/com/example/fabrikatepla/presentation/ui/profile/Menu.kt
+++ app/src/main/java/com/example/fabrikatepla/presentation/ui/profile/Menu.kt
@@ -22,7 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +35,7 @@ import com.example.fabrikatepla.R
 
 @Composable
 fun Menu(supportInfo: SupportInfo, modifier: Modifier = Modifier) {
-    var showBottomSheet by remember { mutableStateOf(false) }
+    var showBottomSheet by rememberSaveable { mutableStateOf(false) }
 
     IconButton(onClick = { showBottomSheet = true }) {
         Icon(
